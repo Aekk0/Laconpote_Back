70136c21ee2c24b5fb8c09c3582a0c295743a0b4
Aekk0 <Franck.Hallaert@gmail.com>
2024-08-04 00:35:24 +0200

    feat(/feature/product/~): use of product with image schema on findById

--- src/feature/product/product.route.ts
+++ src/feature/product/product.route.ts
@@ -4,12 +4,15 @@ import { FastifyInstance } from "fastify";
 // Import Internals
 import { update, create, findOneProduct, findAllProduct, deleteProduct } from "./product.controller";
 import productSchema from "./schema/product.json";
+import pictureSchema from "../picture/schema/picture.json";
+import productWithPictureSchema from "./schema/product-with-picture.json";
 import routeSchema from "./schema/route.json";
 
 
 export default async function api(server: FastifyInstance): Promise<void> {
+    server.addSchema(pictureSchema);
     server.addSchema(productSchema);
-
+    server.addSchema(productWithPictureSchema);
 
     server.patch("/:id", {
         schema: routeSchema.patch
