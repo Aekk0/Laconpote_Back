f370eea71d068114872ec7ec82f7fe5146f56ca2
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2024-09-27 17:22:18 +0200

    feat(paypal): paypal

--- src/feature/payment/payment.controller.ts
+++ src/feature/payment/payment.controller.ts
@@ -1,34 +1,80 @@
 // Import Third-Party Dependencies
 import { FastifyRequest, FastifyReply } from "fastify";
-import { Stripe } from "stripe";
+import * as httpie from "@myunisoft/httpie";
 
 // Import Internal Dependencies
 import { Product } from "../../entities";
+
+// CONSTANTS
+const paypalDomain = process.env.PAYPAL_URI ?? "https://api-m.sandbox.paypal.com";
 
 export type CheckoutRequest = FastifyRequest<{
     Body: Array<Partial<Product> & { quantity: number; }>;
 }>;
 
-export async function checkout(req: CheckoutRequest, res: FastifyReply) {
-    const session = await req.server.stripe.checkout.sessions.create({
-        line_items: [
+// export async function checkout(req: CheckoutRequest, res: FastifyReply) {
+//     const session = await req.server.stripe.checkout.sessions.create({
+//         line_items: [
+//             {
+//                 price_data: {
+//                     currency: "EUR",
+//                     product_data: {
+//                         name: "FOO",
+//                         description: "description",
+//                         // images: [""]
+//                     },
+//                     unit_amount: 1 
+//                 },
+//                 quantity: 1
+//             }
+//         ],
+//         mode: "payment",
+//         success_url: "localhost:4200/payment/success.html",
+//         cancel_url: "localhost:4200/payment/cancel.html"
+//     });
+
+//     res.redirect(303, session.url);
+// }
+
+export async function createOrder(req: CheckoutRequest, res: FastifyReply) {
+    const payload = {
+        intent: "CAPTURE",
+        purchase_units: [
             {
-                price_data: {
-                    currency: "EUR",
-                    product_data: {
-                        name: "FOO",
-                        description: "description",
-                        // images: [""]
-                    },
-                    unit_amount: 1 
-                },
-                quantity: 1
+                amount: {
+                    currency_code: "EUR",
+                    value: 100
+                }
             }
-        ],
-        mode: "payment",
-        success_url: "localhost:4200/payment/success.html",
-        cancel_url: "localhost:4200/payment/cancel.html"
+        ]
+    };
+
+    const postResult = await httpie.safePost(`${paypalDomain}/v2/checkout/orders`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${req.paypalToken}`,
+        },
+        body: JSON.stringify(payload)
     });
 
-    res.redirect(303, session.url);
+    return postResult.unwrap();
+}
+
+export type CaptureOrderRequest = FastifyRequest<{
+    Params: {
+        orderID: string;
+    }
+}>;
+
+export async function captureOrder(req: CaptureOrderRequest) {
+    const orderId = req.params.orderID;
+
+    const postResult = await httpie.post(`${paypalDomain}/v2/checkout/orders/${orderId}/capture`, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${req.paypalToken}`
+        }
+    });
+
+    return postResult;
 }
--- src/feature/payment/payment.route.ts
+++ src/feature/payment/payment.route.ts
@@ -2,10 +2,16 @@
 import { FastifyInstance } from "fastify";
 
 // Import Internal Dependencies
-import { checkout } from "./payment.controller";
+// import { checkout } from "./payment.controller";
+import { paypalReq } from "../../plugin/paypal.plugin";
+import { captureOrder, createOrder } from "./payment.controller";
 
 export default async function api(server: FastifyInstance): Promise<void> {
-    server.post("/checkout-session", checkout);
+    server.register(paypalReq);
+    
+    // server.post("/checkout-session", checkout);
+    server.post("/orders", createOrder);
+    server.post("/orders/:orderID/capture", captureOrder);
 }
 
 export const autoPrefix = "/payment";
